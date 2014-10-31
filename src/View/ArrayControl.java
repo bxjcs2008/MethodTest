31cb8b84a9473ffceae279ba39c28b80bf9ff8f9
xiaolong.wang <smalldragons@163.com>
2014-10-31 10:41:22 +0800

    20141031_am1041

--- src/View/ArrayControl.java
+++ src/View/ArrayControl.java
@@ -24,4 +24,5 @@ public class ArrayControl {
 			type = null;
 		}
 	}
+
 }
--- src/View/GridFrame.java
+++ src/View/GridFrame.java
@@ -1,9 +1,9 @@
 package View;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
@@ -25,7 +25,7 @@ public class GridFrame extends JFrame{
 		
 		//定义面板，并设置为网格布局，4行4列，组件水平、垂直间距均为3 
 		JPanel p=new JPanel(new GridLayout(array.length,array[0].length,4,4));
-		JTextArea t=new JTextArea();        //定义文本框
+		JTextArea t=new JTextArea();        //定义文本框 分数
 		
 		// Key事件
 		KeyListener key = new KeyboardAction();
@@ -36,14 +36,20 @@ public class GridFrame extends JFrame{
 		JButton btn[][];
 		//创建按钮数组
 		btn=new JButton[array.length][array[0].length];
+		
+		
+		
 		//循环定义按钮
-		for (int i = 0 ; i < array.length ; i ++){
+	/*	for (int i = 0 ; i < array.length ; i ++){
 			for (int j = 0 ; j < array[i].length ; j++){
 				btn[i][j]=new JButton(array[i][j]+"");
 				p.add(btn[i][j]);
 				btn[i][j].addKeyListener(key);
 			}
-		}
+		}*/
+		
+		initals(btn,key,p,array);
+		
 		//将面板放置在窗体 CENTER 位置
 		getContentPane().add(p,BorderLayout.CENTER);
 		setVisible(true);
@@ -52,8 +58,22 @@ public class GridFrame extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);               //让窗体居中显示
 		
-
-		
+	}
+	
+	//显示数组内容
+	//设置JButton字体 背景色
+	//位按钮增加监听器
+	public  void initals( JButton [][] btn, KeyListener key,JPanel p, int [][]array){
+		for (int i = 0 ; i < array.length ; i ++){
+			for (int j = 0 ; j < array[i].length ; j++){
+				btn[i][j]=new JButton(array[i][j]+"");
+				Font f=new Font("宋体",Font.BOLD,25);//设置按钮字体，大小，粗细
+				btn[i][j].setBackground(Color.cyan); //设置按钮背景色  青色
+				btn[i][j].setFont(f);
+				p.add(btn[i][j]);
+				btn[i][j].addKeyListener(key);
+			}
+		}
 	}
 	
 	//构造方法
@@ -71,4 +91,5 @@ public class GridFrame extends JFrame{
         	gl.arrayShow(ArrayUtils.array);
         }
         }
+	
 }
