64733000af0eedf4d5330fe8897626eec38dba36
xiaolong.wang <smalldragons@163.com>
2014-10-31 14:55:14 +0800

    20141031_pm1455

---
+++ src/FileCount/CountJavaFile.java
@@ -0,0 +1,15 @@
+package FileCount;
+
+/**
+ * 统计某个项目中java文件的数量
+ * 
+ * */
+public class CountJavaFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+}
